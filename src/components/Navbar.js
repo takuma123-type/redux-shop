f406e580a3f1ed1d0985dbd758783a2f8a69d725
Takuma <>
2023-09-01 11:40:49 +0900

    wip

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import {  CartIcon } from '../HeroIcons';
+import React from "react";
+import { useSelector } from "react-redux";
+import { CartIcon } from "../HeroIcons";
 
 const Navbar = () => {
-    const { amount } = useSelector((store) => store.cart);
-    return <nav>
-        <div className="nav-center">
-            <h3>Redux Shop</h3>
-            <div className="nav-container">
-            <CartIcon />
-                <div className="amount-container">
-                    <p className="total-amount">{amount}</p>
-                </div>
-            </div>
+  const { amount } = useSelector((store) => store.cart);
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3> MIKIHIKO ECショップ</h3>
+        <div className="nav-container">
+          <CartIcon />
+          <div className="amount-container">
+            <p className="total-amount">{amount}</p>
+          </div>
         </div>
+      </div>
     </nav>
-}
+  );
+};
 
 export default Navbar;
